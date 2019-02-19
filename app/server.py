24bd730935061b0946d5e92e5e652a030a292056
zhanglu <18811301719@163.com>
2019-02-19 15:39:00 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -60,6 +60,7 @@ async def analyze(request):
             key=lambda p: p[1],
             reverse=True
         )
+        })
 
 if __name__ == '__main__':
     if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
